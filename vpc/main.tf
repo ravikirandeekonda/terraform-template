53708a06cc41a58d9780a1bb5b5e45421d40efa9
ravikirandeekonda <69287199+ravikirandeekonda@users.noreply.github.com>
2023-06-24 23:04:22 +0530

    Delete vpc directory

--- vpc/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "jenkinsvpc" {
-  cidr_block           = "172.16.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "jenkins-vpc"
-  }
-}
